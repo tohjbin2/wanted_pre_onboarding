bf7b6f0568c2cf29ce09447f1d62be2bb7f42fca
tohjbin <tohjbin@gmail.com>
2022-01-16 00:48:00 +0900

    Add: nav-레이아웃 진행중

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Footer.scss';
 
 function Footer() {
-  return <div />;
+  return <div>footer</div>;
 }
 
 export default Footer;
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,8 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './Nav.scss';
-
+import { BsList } from 'react-icons/bs';
+import { FiSearch, FiBell } from 'react-icons/fi';
 function Nav() {
-  return <div />;
+  const [navCategory, setNavCategory] = useState([]);
+
+  // fetch("/data/navCategory.json", {})
+  //   .then(res => res.json())
+  //   .then(result => {
+  //     if ((result.message = 'SUCCESS')) {
+  //       const newProductList = productList.map(item => {
+  //         if (item.product_id === targetId) {
+  //           return {
+  //             ...item,
+  //             quantity: item.quantity - 1,
+  //           };
+  //         } else {
+  //           return item;
+  //         }
+  //       });
+  //       setProductList(newProductList);
+  //     }
+  //   });
+  return (
+    <div className="nav">
+      <div className="navBox">
+        <div className="logoBox">
+          <span>
+            <img
+              className="listIcon"
+              alt="menu list"
+              src="/images/menu-icon.png"
+            />
+          </span>
+          <img className="logo" alt="logo" src="/images/wanted-logo.jpg" />
+        </div>
+        <div className="categoryBox" />
+        <div className="iconBox">
+          <div>
+            <FiSearch className="searchIcon" />
+          </div>
+          <div>
+            <FiBell className="bellIcon" />
+          </div>
+          <div>
+            <img
+              className="userIcon"
+              alt="user icon"
+              src="/images/user-bin.png"
+            />
+          </div>
+          <div className="serviceBox">
+            <span className="bar">|</span>
+            <Link to="/" className="serviceText">
+              기업 서비스
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Nav;
