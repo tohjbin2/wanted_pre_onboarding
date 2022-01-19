4f8ffd80b3faa7b11898daaa1fcc385701890562
tohjbin <tohjbin@gmail.com>
2022-01-19 17:32:03 +0900

    Add 메인페이지에 슬라이드 이미지 추가

---
+++ src/pages/Main/Slide/Slide.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Slide.scss';
+
+function Slide({ slideImg }) {
+  return (
+    <div className="slide">
+      <img src="https://ifh.cc/g/UN5tb7.jpg" />
+      <img src="https://ifh.cc/g/qJPJ2N.jpg" />
+    </div>
+  );
+}
+
+export default Slide;
